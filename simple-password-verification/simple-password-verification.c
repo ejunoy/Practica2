fd0ea2b8e19904d6ddaa2b1e7294104b0756c2e9
ejunoy <emilio.junoy@ciencias.unam.mx>
2025-03-17 16:35:11 -0600

    Modificar scripts para explotar correctamente la vulnerabilidad

--- simple-password-verification/simple-password-verification.c
+++ simple-password-verification/simple-password-verification.c
@@ -5,24 +5,29 @@
 
 char *login = "root";
 
-int granted() {
-  printf("Lograste llegar hasta aquí. ¡Felicidades!\n");
-  printf("Acceso autorizado...");
-  system("gnome-terminal -x sh -c \"./a.out\"");
-  return 0;
+int granted()
+{
+    printf("Lograste llegar hasta aquí. ¡Felicidades!\n");
+    printf("Acceso Autorizado...\n");
+    // Esta línea podría no funcionar en algunos SO.
+    // Realiza la practica en Kali linux
+    system("gnome-terminal -x sh -c \"./a.out\"");  
+    return 0;
 }
 
-int main() {
-  char password[16];
-  printf("Benvenido\n");
-  printf("Anota la contraseña ");
-  gets(password);
-  
-  if(strcmp(password, login)){
-    printf("Lo siento la contraseña es incorrecta\n Acceso deneagado");
-  }else {
-    granted();
-  }
-
+int main()
+{
+    char password[16];
+    printf("¡Bienvenido!\n");
+    printf("Anota la contraseña por favor: ");
+    gets(password);  
+    
+    if (strcmp(password, login))  
+    {
+        printf("Lo siento la contraseña es incorrecta.\nAcceso Denegado\n");
+    }
+    else
+    {
+        granted();  
+    }
 }
-
